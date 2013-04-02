ce6854a18655e024ee3b1617fc6fe651b5b6d9b7
Brian Caffo <bcaffo@gmail.com>
2013-04-02 10:22:12 -0400

    Lecture 1 R code

--- Lecture 1 code.R
+++ Lecture 1 code.R
@@ -24,5 +24,20 @@ text(mean(x), mean(dnorm(x))+.02, "2.5%", cex = 2)
 text(qnorm(.025), .01, "-1.96", cex = 2)
 text(0, dnorm(0) / 5, "95%", cex = 2)
 
+xval <- seq(-4, 4, length = 1000)
+yval<- dt(xval, 15)
+plot(xval, yval, type = "l", axes = TRUE, frame = FALSE, lwd = 3, xlab = "", ylab = "")
+x <- seq(qt(.975, 15), 4, length = 100)
+polygon(c(x, rev(x)),c(dt(x, 15), rep(0, length(x))), col = "salmon")
+text(mean(x), mean(dt(x, 15))+.02, "2.5%", cex = 2)
+text(qt(.975, 15), .01, "2.13", cex = 2)
+x <- seq(-3.2, qt(.025, 15),length = 100)
+polygon(c(x, rev(x)),c(dt(x, 15), rep(0, length(x))), col = "salmon")
+text(mean(x), mean(dt(x, 15))+.02, "2.5%", cex = 2)
+text(qt(.025, 15), .01, "-2.13", cex = 2)
+text(0, dt(0, 15) / 5, "95%", cex = 2)
+
+
+
 
 
