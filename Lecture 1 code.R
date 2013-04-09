fa44ee1019da73f6ee2ed20207442be8337716cc
Brian Caffo <bcaffo@gmail.com>
2013-04-09 11:56:31 -0400

    Added Lecture 4

--- Lecture 1 code.R
+++ Lecture 1 code.R
@@ -31,13 +31,20 @@ x <- seq(qt(.975, 15), 4, length = 100)
 polygon(c(x, rev(x)),c(dt(x, 15), rep(0, length(x))), col = "salmon")
 text(mean(x), mean(dt(x, 15))+.02, "2.5%", cex = 2)
 text(qt(.975, 15), .01, "2.13", cex = 2)
-x <- seq(-3.2, qt(.025, 15),length = 100)
+x <- seq(-4, qt(.025, 15),length = 100)
 polygon(c(x, rev(x)),c(dt(x, 15), rep(0, length(x))), col = "salmon")
 text(mean(x), mean(dt(x, 15))+.02, "2.5%", cex = 2)
 text(qt(.025, 15), .01, "-2.13", cex = 2)
 text(0, dt(0, 15) / 5, "95%", cex = 2)
 
-
+pt(.8, 15, lower.tail=FALSE)
+xval <- seq(-4, 4, length = 1000)
+yval<- dt(xval, 15)
+plot(xval, yval, type = "l", axes = TRUE, frame = FALSE, lwd = 3, xlab = "", ylab = "")
+x <- seq(.8, 4, length = 100)
+polygon(c(x, rev(x)),c(dt(x, 15), rep(0, length(x))), col = "salmon")
+text(mean(x), mean(dt(x, 15))+.02, "22%", cex = 2)
+text(0.8, .01, "0.8", cex = 2)
 
 
 
